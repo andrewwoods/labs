9edd4373595b13d29e370c10b73c911157f4cda7
Andrew Woods <atwoods1@gmail.com>
2015-11-16 22:57:24 -0800

    Add search results page
    Create a template to represent what will come back from the server.

---
+++ offline-app/search-results.php
@@ -0,0 +1,10 @@
+
+<div id="profile-1" class="profile-result">
+	<img width="90" src="library/images/nothumb.gif">
+	<h3>Firstname Lastname</h3>
+	<div id="profile-1-content" class="is-closed">
+		<p>I'm good at ... <p>
+		<p>I'm looking for ... <p>
+	</div>
+</div>
+
