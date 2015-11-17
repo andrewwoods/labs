645232c5bed025e14d30d9d31580f9010426db72
Andrew Woods <atwoods1@gmail.com>
2015-11-16 23:33:53 -0800

    Minor tweaks for improve layouts

--- offline-app/search-results.php
+++ offline-app/search-results.php
@@ -1,8 +1,8 @@
 
-<div id="profile-1" class="profile-result">
+<div id="profile-1" class="profile-result clearfix">
 	<img width="90" src="library/images/nothumb.gif">
 	<h3>Firstname Lastname</h3>
-	<div id="profile-1-content" class="is-closed">
+	<div id="profile-1-content" class="text-content is-closed">
 		<p>I'm good at ... <p>
 		<p>I'm looking for ... <p>
 	</div>
