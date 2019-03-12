4773490d9e03d8fe4be7e9f1177b53d3f01de16d
Andrew Woods <atwoods1@gmail.com>
2019-03-12 12:28:44 -0700

    Create separate header and footer template files
    Makes it easier to create a consistent layout across multiple pages

---
+++ resources/templates/header.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Labs</title>
+	<meta charset="UTF-8">
+	<meta name="author" content="Andrew Woods">
+	<link rel="stylesheet" href="css/home.css">
+</head>
+<body>
+
+<div id="page">
+<div id="page-content" class="content">
+	<main id="main">
+		<div id="main-content" class="content">
--- resources/templates/index.php
+++ resources/templates/index.php
@@ -1,47 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<title>Labs</title>
-	<meta charset="UTF-8">
-	<meta name="author" content="Andrew Woods">
-	<link rel="stylesheet" href="css/home.css">
-</head>
-<body>
+<?php include "header.php"; ?>
 
-<div id="page">
-<div id="page-content" class="content">
-	<main id="main">
-		<div id="main-content" class="content">
-			<h1>Labs</h1>
-			<p>A Collection of fun and exploratory design ideas. Each design 
-			usually consists of a single page, but may have more depending upon
-			the concept. 
-			<span class="tablet-content">The point of each concept is to try 
-			something new, to not play it safe. Try a new browser feature or idea.
-			</span>
-			<span class="desktop-content">Each concept created here is an opprortunity 
-			you might not have otherwise.</span></p>
+            <h1>Labs</h1>
+            <p>A Collection of fun and exploratory design ideas. Each design
+            usually consists of a single page, but may have more depending upon
+            the concept.
+            <span class="tablet-content">The point of each concept is to try
+            something new, to not play it safe. Try a new browser feature or idea.
+            </span>
+            <span class="desktop-content">Each concept created here is an opprortunity
+            you might not have otherwise.</span></p>
 
-			<hr class="small" />
-			<p>Create a labs subdomain of your website, and start building something different.</p>
+            <hr class="small" />
+            <p>Create a labs subdomain of your website, and start building something different.</p>
 
-		</div>
+            </div>
 
-		<div class="experiments">
-			<div class="experiment">
-				<h2><a href="transparent-text/">Transparent Text</a></h2>
-				<img src="images/sample.png" alt="Transparent Text">
-				<p>uses blend mode to display an image through the text.</p>
-			</div>
-			<div class="experiment">
-				<h2><a href="https://github.com/andrewwoods/opal">Opal</a></h2>
-				<img src="images/sample.png" alt="Opal">
-				<p>A toolkit for command line power users.</p>
-			</div>
-		</div>
-	</main>
-</div>
-</div>
+            <div class="experiments">
+            <div class="experiment">
+                <h2><a href="transparent-text/">Transparent Text</a></h2>
+                <img src="images/sample.png" alt="Transparent Text">
+                <p>uses blend mode to display an image through the text.</p>
+            </div>
+            <div class="experiment">
+                <h2><a href="https://github.com/andrewwoods/opal">Opal</a></h2>
+                <img src="images/sample.png" alt="Opal">
+                <p>A toolkit for command line power users.</p>
+            </div>
 
-</body>
-</html>
+<?php include "footer.php";
